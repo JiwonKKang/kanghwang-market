9b7c21d3418835514cd3baf36bd0c3fed7641bf3
강지원 <kangji149@naver.com>
2023-12-18 22:32:04 +0900

    feat: add swagger-ui
    Swagger-UI를 통한 API 명세

--- src/main/java/com/core/market/trade/api/TradePostController.java
+++ src/main/java/com/core/market/trade/api/TradePostController.java
@@ -2,10 +2,16 @@ package com.core.market.trade.api;
 
 import com.core.market.common.Response;
 import com.core.market.trade.api.request.TradePostCreateRequest;
+import com.core.market.trade.api.response.PostDetailResponse;
 import com.core.market.trade.api.response.TradePostResponse;
 import com.core.market.trade.app.TradePostService;
 import com.core.market.trade.domain.TradePost;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,6 +22,7 @@ public class TradePostController {
 
     private final TradePostService tradePostService;
 
+    @Tag(name = "TradePost API",description = "거래글 API입니다.")
     @PostMapping
     public Response<Void> createTradePost(TradePostCreateRequest request) {
         tradePostService.createTradePost(request);
@@ -23,8 +30,17 @@ public class TradePostController {
     }
 
     @GetMapping("/{postId}")
-    public Response<TradePostResponse> getTradePost(@PathVariable Long postId) {
-        return Response.success(tradePostService.getTradePost(postId));
+    public Response<PostDetailResponse> getTradePost(@PathVariable Long postId) {
+        return Response.success(PostDetailResponse.from(tradePostService.getTradePost(postId)));
     }
 
+
+    @GetMapping
+    public Response<Page<TradePostResponse>> getTradePostPage(
+            @PageableDefault(size = 9, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable) {
+
+        Page<TradePostResponse> responses = tradePostService.getTradePostPage(pageable)
+                .map(TradePostResponse::from);
+        return Response.success(responses);
+    }
 }
--- src/main/java/com/core/market/trade/api/request/TradePostCreateRequest.java
+++ src/main/java/com/core/market/trade/api/request/TradePostCreateRequest.java
@@ -1,13 +1,22 @@
 package com.core.market.trade.api.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
 public record TradePostCreateRequest(
+
+        @Schema(description = "거래글 제목", nullable = false, example = "옷 판매합니다.")
         String title,
+
+        @Schema(description = "거래글 내용", nullable = false)
         String content,
+
+        @Schema(description = "가격", nullable = false, example = "35000")
         Integer price,
+
+        @Schema(description = "제품 사진", nullable = true)
         List<MultipartFile> files
 ) {
 }
